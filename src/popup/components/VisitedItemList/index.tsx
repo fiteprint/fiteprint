9ac604a5a08d1b959ab041d07df64ba644f92514
wildseeder <wildseeder@gmail.com>
2020-10-01 23:40:46 +0800

    feat: empty status

--- src/popup/components/VisitedItemList/index.tsx
+++ src/popup/components/VisitedItemList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { List, ListRowRenderer } from 'react-virtualized';
 
 import Item, { ItemData } from './Item';
@@ -14,6 +15,13 @@ const ITEM_HEIGHT = 50;
 const MAX_HEIGHT = 400;
 const WIDTH = 400;
 
+const Empty = styled.div`
+  padding: 1em 0;
+  width: ${WIDTH}px;
+  text-align: center;
+  color: #999;
+`;
+
 export default function VisitedItemList(props: Props): JSX.Element {
   const rowRenderer: ListRowRenderer = ({ key, index, style }) => (
     <div key={key} style={style}>
@@ -21,17 +29,23 @@ export default function VisitedItemList(props: Props): JSX.Element {
     </div>
   );
   return (
-    <List
-      width={WIDTH}
-      height={Math.min(MAX_HEIGHT, props.total * ITEM_HEIGHT)}
-      rowHeight={ITEM_HEIGHT}
-      rowCount={props.items.length}
-      rowRenderer={rowRenderer}
-      style={{
-        outline: 0,
-        padding: '8px 0',
-        boxSizing: 'content-box',
-      }}
-    />
+    <>
+      {props.total > 0 ?
+        <List
+          width={WIDTH}
+          height={Math.min(MAX_HEIGHT, props.total * ITEM_HEIGHT)}
+          rowHeight={ITEM_HEIGHT}
+          rowCount={props.items.length}
+          rowRenderer={rowRenderer}
+          style={{
+            outline: 0,
+            padding: '8px 0',
+            boxSizing: 'content-box',
+          }}
+        />
+        :
+        <Empty>{chrome.i18n.getMessage('noBrowsingHistory')}</Empty>
+      }
+    </>
   );
 }
